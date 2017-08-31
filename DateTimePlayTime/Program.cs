21fd9d369140def1ac272bb5c3782e01831af972
Sam Jacobson <samuel.c.jacobson@gmail.com>
2017-08-31 17:23:58 -0400

    additional problems

--- DateTimePlayTime/Program.cs
+++ DateTimePlayTime/Program.cs
@@ -33,6 +33,38 @@ namespace DateTimePlayTime
                     Console.WriteLine(year + " is a leap year.");
                 else
                     Console.WriteLine(year + " is NOT a leap year.");
+
+            //Write a program to calculate what day of the week is 40 days from this moment.
+            DateTime today = DateTime.Now;
+            DateTime forty = today.AddDays(40);
+            Console.WriteLine("40 days from now will be a " + forty.DayOfWeek + ".");
+
+            //Write a program to get the difference between two dates in days.
+            //For example, the difference between June 3, 2017 and June 5, 2017 is 2 days.
+            DateTime date1 = new DateTime(2017, 6, 3);
+            DateTime date2 = new DateTime(2017, 6, 5);
+            Console.WriteLine("The difference between " + date1.ToShortDateString() + " and " + date2.ToShortDateString() + " is " 
+                + (date2 - date1).TotalDays + " days.");
+
+            //Write a program to check whether the given year, month and day are the current or not. 
+            //Test Data : Input the Day: 17 Input the Month: 09 Input the Year: 2017
+            //The formatted Date is : 17 / 09 / 2016
+            //The current date status : False
+            Console.WriteLine("Input the day:");
+            int day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the month:");
+            int month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Input the year:");
+            int year2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("The formatted date is: " + day + " / " + month + " / " + year2);
+
+            bool dateStatus;
+            if (DateTime.Now.Day == day && DateTime.Now.Month == month && DateTime.Now.Year == year2)
+                dateStatus = true;
+            else
+                dateStatus = false;
+            
+            Console.WriteLine("The current date status: " + dateStatus);
         }
     }
 }
